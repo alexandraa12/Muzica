e51db2bff38e3950df6bc3aff4f4740c0ea8a681
alexandraa12 <168365581+alexandraa12@users.noreply.github.com>
2024-04-28 20:48:16 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,14 @@
+function openpornire() {
+    window.location.href='index.html';
+
+}
+function openarticles() {
+    window.location.href='2.html';
+
+}
+function openmagazin() {
+    window.location.href='3.html';
+}
+function openevenimente() {
+    window.location.href='4.html';
+}
